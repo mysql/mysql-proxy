491371e628c372187840cd1db54ae4ef776b215e
jkneschke <>
2007-07-09 23:37:58 +0100

    added a example how connection splitting will work

---
+++ trunk/examples/tutorial-keepalive.lua
@@ -0,0 +1,35 @@
+
+local is_in_transaction = 0
+
+function read_query( packet ) 
+	if packet:byte() == proxy.COM_QUIT then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_ERR,
+			errmsg = "ignored the COM_QUIT"
+		}
+
+		return proxy.PROXY_SEND_RESULT
+	end
+
+	proxy.queries:append(1, packet)
+
+	if is_in_transaction == 0 and
+	   packet:byte() == proxy.COM_QUERY and
+	   packet:sub(2, 7) == "SELECT" then
+		-- send to slave
+		print("sending to slave");
+	else
+		-- send to master
+		print("sending to master");
+	end
+
+	return proxy.PROXY_SEND_QUERY
+end
+
+function read_query_result( inj ) 
+	local res      = assert(inj.resultset)
+  	local flags    = res.flags
+
+	is_in_transaction = flags.in_trans
+end
+
