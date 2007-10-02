aaaf68283f702dd935f76e0ac7475ef8db9b1bed
gmaxia <>
2007-10-02 17:37:42 +0100

    Added more tests on packet error messages

--- trunk/tests/suite/base/t/error_struct.lua
+++ trunk/tests/suite/base/t/error_struct.lua
@@ -3,13 +3,13 @@ function read_query( packet )
 	if packet:byte() ~= proxy.COM_QUERY then 
 		return 
 	end
-    if packet:sub(2):lower():match('^%s*select%s+error') then
-		local errno = 3306
-		local sql_state = 'S12XYZ'
+    local err_code,sql_state, err_msg = 
+        packet:sub(2):lower():match('^%s*select%s+error%s+(%d+)%s+(%w+)%s+"(.*)"') 
+    if (err_code and sql_state) then
 		proxy.response = { 
 			type     = proxy.MYSQLD_PACKET_ERR, 
-			errmsg   = 'FAKE ERROR MESSAGE', 
-			errcode  = errno, 
+			errmsg   = err_msg,
+			errcode  = err_code, 
 			sqlstate = sql_state
 		}
 		return proxy.PROXY_SEND_RESULT
