dca13f91949d576240f16fa9827b66fe219c2929
Jan <Jan@JAN-N76T46VCN4S>
2008-03-28 00:03:29 +0100

    fixed the libdir=... on win32

--- trunk/cmake/ChassisPlugin.cmake
+++ trunk/cmake/ChassisPlugin.cmake
@@ -2,7 +2,12 @@ MACRO(CHASSIS_PLUGIN _plugin_name)
 	SET(_la_name "${CMAKE_SHARED_LIBRARY_PREFIX}${_plugin_name}.la")
 	SET(_so_name "${CMAKE_SHARED_LIBRARY_PREFIX}${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}")
 	FILE(WRITE ${_la_name} "dlname=\"${_so_name}\"\n")
-	FILE(APPEND ${_la_name} "libdir=\"lib/\"\n")
+	IF(WIN32)
+		FILE(APPEND ${_la_name} "libdir=\".\\\\\"\n")
+	ELSE(WIN32)
+		FILE(APPEND ${_la_name} "libdir=\"./lib/\"\n")
+	ENDIF(WIN32)
+
 	FILE(APPEND ${_la_name} "installed=yes\n")
 	
 	INSTALL(FILES ${_la_name} DESTINATION lib)
