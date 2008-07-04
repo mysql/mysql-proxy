920c0c8b64abda4209b2ad916ddc0fb371a033c8
jan@mysql.com <>
2008-07-04 11:13:35 +0100

    added test-cases for simple fail-over detection
    * still needs support for multiple backends

---
+++ tests/suite/base/t/failover-mock.lua
@@ -0,0 +1,75 @@
+---
+-- test if failover works
+--
+--
+
+require("chassis")
+
+function packet_auth(fields)
+	fields = fields or { }
+	return "\010" ..             -- proto version
+		(fields.version or "5.0.45-proxy") .. -- version
+		"\000" ..             -- term-null
+		"\001\000\000\000" .. -- thread-id
+		"\065\065\065\065" ..
+		"\065\065\065\065" .. -- challenge - part I
+		"\000" ..             -- filler
+		"\001\130" ..         -- server cap (long pass, 4.1 proto)
+		"\008" ..             -- charset
+		"\002\000" ..         -- status
+		("\000"):rep(13) ..   -- filler
+		"\065\065\065\065"..
+		"\065\065\065\065"..
+		"\065\065\065\065"..
+		"\000"                -- challenge - part II
+end
+
+function connect_server()
+	-- emulate a server
+	proxy.response = {
+		type = proxy.MYSQLD_PACKET_RAW,
+		packets = {
+			packet_auth()
+		}
+	}
+	return proxy.PROXY_SEND_RESULT
+end
+
+function read_query(packet)
+	if packet:byte() == proxy.COM_SHUTDOWN then
+		chassis.set_shutdown()
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_RAW,
+			packets = { string.char(254) },
+		}
+		return proxy.PROXY_SEND_RESULT
+	elseif packet:byte() ~= proxy.COM_QUERY then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_OK
+		}
+		return proxy.PROXY_SEND_RESULT
+	end
+
+	local query = packet:sub(2) 
+	if query == 'SELECT 1' then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_OK,
+			resultset = {
+				fields = {
+					{ name = '1' },
+				},
+				rows = { { 1 } }
+			}
+		}
+	else
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_ERR,
+			errmsg = "(pooling-mock) " .. query
+		}
+	end
+	return proxy.PROXY_SEND_RESULT
+end
+
+
+
+
