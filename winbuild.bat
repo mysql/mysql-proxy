92b7d95591a9e2dbe9695a6d70458d08b49e48ab
Kay Roepke <kay@mysql.com>
2008-10-28 19:29:36 +0100

    add openssl support for curl on windows

--- winbuild.bat
+++ winbuild.bat
@@ -50,9 +50,6 @@
 
 @echo Copying dependencies to deps folder
 @copy %DEPS_PATH%\packages\* deps\
-@echo Special cURL+SSL binary installation
-@mkdir deps\curlssl
-@copy %DEPS_PATH%\ssl-binary-libs\*.dll deps\curlssl
 
 @rem clear the cache if neccesary to let cmake recheck everything
 @rem del CMakeCache.txt
