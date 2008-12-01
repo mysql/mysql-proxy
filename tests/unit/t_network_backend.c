d708a2e2bc9ca2e8df0c11cd7ac8673da3ca11a4
jan@mysql.com <>
2008-12-01 11:35:45 +0100

    reproduce the failing gatekeeper on network_backends_check() that has led to never set a backend from _DOWN to _UNKOWN and leaving disabled backends in the disabled state forever

--- tests/unit/t_network_backend.c
+++ tests/unit/t_network_backend.c
@@ -72,6 +72,56 @@ void t_network_backends_add() {
 	network_backends_free(backends);
 }
 
+/**
+ * check if the timeout handle of backends_check() works 
+ *
+ * it should remove the _DOWN state back to something reasonable after 4 seconds 
+ */
+void t_network_backends_check() {
+	network_backends_t *backends;
+	backend_t *backend;
+
+	backends = network_backends_new();
+	g_assert(backends);
+	
+	g_assert_cmpint(network_backends_add(backends, "127.0.0.1", BACKEND_TYPE_RW), ==, 0);
+
+	/* setup the test 
+	 *
+	 * mark the backend as down */
+	backend = network_backends_get(backends, 0);
+	backend->state = BACKEND_STATE_DOWN;
+	g_get_current_time(&backend->state_since);
+	
+	/* we have to wait 4sec before it goes from DOWN to UNKNOWN */
+	g_assert_cmpint(0, ==, network_backends_check(backends));
+	
+	/* travel back in time and pretend this state is already 5 sec old */
+	backend->state_since.tv_sec -= 5;
+	
+	/* we should hit the gatekeeper to reduce the number of calls to iterate the 
+	 * all backends */
+	g_assert_cmpint(0, ==, network_backends_check(backends));
+
+	/* open the gate */
+	backends->backend_last_check.tv_sec -= 1; /* pretend we checked last 1sec ago */
+
+ 	/* we havn't checked any backends yet, so our backend should be set to _UNKNOWN */
+	g_assert_cmpint(1, ==, network_backends_check(backends));
+
+	g_assert_cmpint(BACKEND_STATE_UNKNOWN, ==, backend->state);
+
+	/* set it to down again and check if we the caching works */
+	backend->state = BACKEND_STATE_DOWN; 
+	backend->state_since.tv_sec -= 5;
+
+ 	/* as long as we are below 1sec, no updates */
+	g_assert_cmpint(0, ==, network_backends_check(backends));
+	g_assert_cmpint(BACKEND_STATE_DOWN, ==, backend->state);
+
+	network_backends_free(backends);
+}
+
 int main(int argc, char **argv) {
 	g_thread_init(NULL);
 	g_test_init(&argc, &argv, NULL);
@@ -80,6 +130,7 @@ int main(int argc, char **argv) {
 	g_test_add_func("/core/network_backends_new", t_network_backends_new);
 	g_test_add_func("/core/network_backend_new", t_network_backend_new);
 	g_test_add_func("/core/network_backends_add", t_network_backends_add);
+	g_test_add_func("/core/network_backends_check", t_network_backends_check);
 
 	return g_test_run();
 }
