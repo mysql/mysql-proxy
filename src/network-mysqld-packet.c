549c58dae7f0afa4e6f58fd54822dce1066dcc83
Joshua Zhu <josh@joshua>
2009-02-05 10:53:19 +0800

    fix compile warnings: network-mysqld-packet.c: In function ‘network_mysqld_proto_get_fielddefs’: network-mysqld-packet.c:685: warning: passing argument 2 of ‘network_mysqld_proto_get_int16’ from incompatible pointer type network-mysqld-packet.c:686: warning: passing argument 2 of ‘network_mysqld_proto_get_int32’ from incompatible pointer type network-mysqld-packet.c:687: warning: passing argument 2 of ‘network_mysqld_proto_get_int8’ from incompatible pointer type network-mysqld-packet.c:688: warning: passing argument 2 of ‘network_mysqld_proto_get_int16’ from incompatible pointer type network-mysqld-packet.c:689: warning: passing argument 2 of ‘network_mysqld_proto_get_int8’ from incompatible pointer type network-mysqld-packet.c:701: warning: passing argument 2 of ‘network_mysqld_proto_get_int24’ from incompatible pointer type network-mysqld-packet.c:704: warning: passing argument 2 of ‘network_mysqld_proto_get_int8’ from incompatible pointer type network-mysqld-packet.c:707: warning: passing argument 2 of ‘network_mysqld_proto_get_int16’ from incompatible pointer type network-mysqld-packet.c:708: warning: passing argument 2 of ‘network_mysqld_proto_get_int8’ from incompatible pointer type network-mysqld-packet.c:712: warning: passing argument 2 of ‘network_mysqld_proto_get_int8’ from incompatible pointer type

--- src/network-mysqld-packet.c
+++ src/network-mysqld-packet.c
@@ -682,11 +682,11 @@ GList *network_mysqld_proto_get_fielddefs(GList *chunk, GPtrArray *fields) {
 	        
 			err = err || network_mysqld_proto_skip(&packet, 1); /* filler */
 	        
-			err = err || network_mysqld_proto_get_int16(&packet, &field->charsetnr);
-			err = err || network_mysqld_proto_get_int32(&packet, &field->length);
-			err = err || network_mysqld_proto_get_int8(&packet, &field->type);
-			err = err || network_mysqld_proto_get_int16(&packet, &field->flags);
-			err = err || network_mysqld_proto_get_int8(&packet, &field->decimals);
+			err = err || network_mysqld_proto_get_int16(&packet, (guint16 *)&field->charsetnr);
+			err = err || network_mysqld_proto_get_int32(&packet, (guint32 *)&field->length);
+			err = err || network_mysqld_proto_get_int8(&packet, (guint8 *)&field->type);
+			err = err || network_mysqld_proto_get_int16(&packet, (guint16 *)&field->flags);
+			err = err || network_mysqld_proto_get_int8(&packet, (guint8 *)&field->decimals);
 	        
 			err = err || network_mysqld_proto_skip(&packet, 2); /* filler */
 		} else {
@@ -698,18 +698,18 @@ GList *network_mysqld_proto_get_fielddefs(GList *chunk, GPtrArray *fields) {
 			err = err || network_mysqld_proto_get_lenenc_string(&packet, &field->name, NULL);
 			err = err || network_mysqld_proto_get_int8(&packet, &len);
 			err = err || (len != 3);
-			err = err || network_mysqld_proto_get_int24(&packet, &field->length);
+			err = err || network_mysqld_proto_get_int24(&packet, (guint32 *)&field->length);
 			err = err || network_mysqld_proto_get_int8(&packet, &len);
 			err = err || (len != 1);
-			err = err || network_mysqld_proto_get_int8(&packet, &field->type);
+			err = err || network_mysqld_proto_get_int8(&packet, (guint8 *)&field->type);
 			err = err || network_mysqld_proto_get_int8(&packet, &len);
 			if (len == 3) { /* the CLIENT_LONG_FLAG is set */
-				err = err || network_mysqld_proto_get_int16(&packet, &field->flags);
-				err = err || network_mysqld_proto_get_int8(&packet, &field->decimals);
+				err = err || network_mysqld_proto_get_int16(&packet, (guint16 *)&field->flags);
+				err = err || network_mysqld_proto_get_int8(&packet, (guint8 *)&field->decimals);
 			} else if (len == 2) {
 				guint8 flags = 0;
 				err = err || network_mysqld_proto_get_int8(&packet, &flags);
-				err = err || network_mysqld_proto_get_int8(&packet, &field->decimals);
+				err = err || network_mysqld_proto_get_int8(&packet, (guint8 *)&field->decimals);
 
 				field->flags = flags;
 			} else {
