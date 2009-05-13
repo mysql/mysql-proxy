f374dc8994dffe1355204a26881c0fdb6895a30c
jan@mysql.com <>
2009-05-13 14:01:01 +0200

    added a (failing) test to reproduce #41991

---
+++ tests/suite/base/t/bug_41991-mock.lua
@@ -0,0 +1,44 @@
+--[[ $%BEGINLICENSE%$
+ Copyright (C) 2008 MySQL AB, 2008 Sun Microsystems, Inc
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; version 2 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ $%ENDLICENSE%$ --]]
+local proto = require("mysql.proto")
+
+function connect_server()
+	proxy.response = {
+		type = proxy.MYSQLD_PACKET_RAW,
+		packets = {
+			proto.to_challenge_packet({})
+		}
+	}
+
+	return proxy.PROXY_SEND_RESULT
+end
+
+function read_query(packet)
+	if packet:byte() ~= proxy.COM_REFRESH then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_OK
+		}
+		return proxy.PROXY_SEND_RESULT
+	end
+
+	proxy.response = {
+		type = proxy.MYSQLD_PACKET_ERR,
+		errmsg = "(bug_35669-mock) >" .. packet:sub(2) .. "<"
+	}
+	return proxy.PROXY_SEND_RESULT
+end
