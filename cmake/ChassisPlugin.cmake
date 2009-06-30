c046cb5214c61fc117df5004bfecf9e378100bc1
jan@mysql.com <>
2009-06-30 14:01:38 +0200

    the cmake win32 build adds the /Release to the path

--- cmake/ChassisPlugin.cmake
+++ cmake/ChassisPlugin.cmake
@@ -4,7 +4,7 @@ MACRO(CHASSIS_PLUGIN_INSTALL _plugin_name)
 			DESTINATION lib/mysql-proxy/plugins)
 	ELSE(NOT WIN32)
 		## on win32 the lua module gets prefixed with lua- and end up in bin/
-		INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
+		INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${CMAKE_SHARED_LIBRARY_PREFIX}${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
 			DESTINATION bin/
 			RENAME plugin-${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
 		)
