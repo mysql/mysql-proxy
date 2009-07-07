56ab410e1428260fc6c3fd8b34642fb579d8e4e0
jan@mysql.com <>
2009-07-07 16:18:55 +0200

    extended the test to cover the fields too

--- tests/suite/base/t/bug_35729-test.lua
+++ tests/suite/base/t/bug_35729-test.lua
@@ -22,17 +22,26 @@ end
 -- side gets the wrong values
 --
 function read_query_result(inj)
+	local fields = inj.resultset.fields
+	collectgarbage("collect") -- trigger a full GC
+
+	assert(type(fields) == "userdata")
+	assert(type(fields[1]) == "userdata") -- if the GC removed the underlying c-struct, fields[1] will be nil 
+
 	proxy.response = {
 		type = proxy.MYSQLD_PACKET_OK,
 		resultset = {
-			fields = {
-				{ name = "Name", type = proxy.MYSQL_TYPE_STRING },
-				{ name = "Value", type = proxy.MYSQL_TYPE_STRING }
-			},
+			fields = { },
 			rows = { }
 		}
 	}
 
+	proxy.response.resultset.fields[1] = {
+		name = fields[1].name, type = fields[1].type
+	}
+	proxy.response.resultset.fields[2] = {
+		name = fields[2].name, type = fields[2].type
+	}
 	for row in inj.resultset.rows do
 		collectgarbage("collect") -- trigger a full GC
 
