5e870a33b3d5043bdcff455388fb4292fd938b36
jan@mysql.com <>
2009-08-31 17:42:54 +0200

    added failing test for #46141

---
+++ tests/suite/base/t/bug_46141-mock.lua
@@ -0,0 +1,59 @@
+--[[ $%BEGINLICENSE%$
+ Copyright (C) 2008 MySQL AB, 2008 Sun Microsystems, Inc
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; version 2 of the License.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ $%ENDLICENSE%$ --]]
+local proto = require("mysql.proto")
+
+function connect_server()
+	proxy.response = {
+		type = proxy.MYSQLD_PACKET_RAW,
+		packets = {
+			proto.to_challenge_packet({})
+		}
+	}
+
+	return proxy.PROXY_SEND_RESULT
+end
+
+function read_query(packet)
+	if packet:byte() ~= proxy.COM_QUERY then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_OK
+		}
+		return proxy.PROXY_SEND_RESULT
+	end
+
+	if packet:sub(2) == "SELECT 1" then
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_OK,
+			resultset = {
+				fields = {
+					{ name = "1", type = proxy.MYSQL_TYPE_STRING },
+				},
+				rows = {
+					{ "1" }
+				}
+			}
+		}
+		return proxy.PROXY_SEND_RESULT
+	else 
+		proxy.response = {
+			type = proxy.MYSQLD_PACKET_ERR,
+			errmsg = "(bug_41991-mock) >" .. packet:sub(2) .. "<"
+		}
+		return proxy.PROXY_SEND_RESULT
+	end
+end
