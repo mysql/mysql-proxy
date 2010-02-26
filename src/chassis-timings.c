acefa99240ea615ac59a64bdc55f55776916e60f
michael.schuster@sun.com <>
2010-02-26 00:14:18 -0800

    fix copyright (year)

--- src/chassis-timings.c
+++ src/chassis-timings.c
@@ -1,5 +1,5 @@
 /* $%BEGINLICENSE%$
- Copyright (C) 2010 Sun Microsystems, Inc
+ Copyright (C) 2009-2010 Sun Microsystems, Inc
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
