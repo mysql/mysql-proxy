f0b252c73443d7fc91e7bf1188fc6b61010f517c
jan.kneschke@oracle.com <>
2012-02-21 18:28:43 +0100

    post-merge fix
      - add missing )
      - make rel_plugin_dir const and move it into the if-block

--- src/chassis-frontend.c
+++ src/chassis-frontend.c
@@ -308,7 +308,6 @@ int chassis_frontend_init_lua_cpath(const char *set_path, const char *base_dir, 
 
 int chassis_frontend_init_plugin_dir(char **_plugin_dir, const char *base_dir) {
 	char *plugin_dir = *_plugin_dir;
-	char *rel_plugin_dir;
 
 	if (plugin_dir) return 0;
 
@@ -317,8 +316,8 @@ int chassis_frontend_init_plugin_dir(char **_plugin_dir, const char *base_dir) {
 
 #elif defined(PLUGINDIR) && defined(EXEC_PREFIX)
 
-	if (chassis_path_string_is_parent_of(C(EXEC_PREFIX), C(PLUGINDIR)) {
-		rel_plugin_dir = PLUGINDIR + sizeof(EXEC_PREFIX) - 1;
+	if (chassis_path_string_is_parent_of(C(EXEC_PREFIX), C(PLUGINDIR))) {
+		const char *rel_plugin_dir = PLUGINDIR + sizeof(EXEC_PREFIX) - 1;
 
 		if (rel_plugin_dir[0] == G_DIR_SEPARATOR) rel_plugin_dir++; /* if plugindir starts with a /, skip it */
 
