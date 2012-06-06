88c72c848ac1c2bd1f206e744bfa5711046a8ff3
jan.kneschke@oracle.com <>
2012-06-06 14:14:20 +0200

    replace $(Outdir) before we pass it down

--- cmake/ChassisPlugin.cmake
+++ cmake/ChassisPlugin.cmake
@@ -8,6 +8,7 @@ MACRO(CHASSIS_PLUGIN_INSTALL _plugin_name)
 	ELSE(NOT WIN32)
 		## on win32 the chassis plugins gets prefixed with plugin- and end up in bin/
 		GET_TARGET_PROPERTY(built_location ${_plugin_name} LOCATION)
+		STRING(REPLACE "$(OutDir)" "${CMAKE_BUILD_TYPE}" built_location ${built_location})
 		INSTALL(FILES ${built_location}
 			DESTINATION bin/
 			RENAME plugin-${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
