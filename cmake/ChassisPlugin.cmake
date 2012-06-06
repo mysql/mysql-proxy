b97507256e369b5adbf6a5bd7732706a3de6b244
jan.kneschke@oracle.com <>
2012-06-06 13:33:48 +0200

    install the .pdb's of a .dll too

--- cmake/ChassisPlugin.cmake
+++ cmake/ChassisPlugin.cmake
@@ -6,11 +6,20 @@ MACRO(CHASSIS_PLUGIN_INSTALL _plugin_name)
 		INSTALL(TARGETS ${_plugin_name}
 			DESTINATION lib/mysql-proxy/plugins)
 	ELSE(NOT WIN32)
-		## on win32 the lua module gets prefixed with lua- and end up in bin/
-		INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${CMAKE_SHARED_LIBRARY_PREFIX}${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
+		## on win32 the chassis plugins gets prefixed with plugin- and end up in bin/
+		GET_TARGET_PROPERTY(built_location ${_plugin_name} LOCATION)
+		INSTALL(FILES ${built_location}
 			DESTINATION bin/
 			RENAME plugin-${_plugin_name}${CMAKE_SHARED_LIBRARY_SUFFIX}
 		)
+		## install the .pdb too
+		IF(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
+			STRING(REPLACE ${CMAKE_SHARED_LIBRARY_PREFIX} ".pdb" pdb_location ${built_location})
+			INSTALL(FILES
+				${pdb_location}
+				DESTINATION bin
+			)
+		ENDIF()
 	ENDIF(NOT WIN32)
 ENDMACRO(CHASSIS_PLUGIN_INSTALL _plugin_name)
 
